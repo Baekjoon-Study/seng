82144858fd7858c7db4a1cb3d6f40683cb834e10
seEuN <parkse1010@gmail.com>
2024-09-24 13:46:45 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.c
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main(){
-    int num,i;
+    int n,i;
     int students[31] = {0,};
     
     for(i=0;i<28;i++){
-        scanf("%d\n",&num);
-        students[num] = num;
+        scanf("%d\n", &n);
+        students[n] = n;
     }
+    
     for(i=1;i<31;i++){
-        if(students[i]==0)
+        if(students[i]==0) 
             printf("%d\n", i);
     }
     return 0;
